ecc1ff1cd617003788e4a4295c1153f653e01216
Johan Lorenzo <jlorenzo@mozilla.com>
2018-08-20 14:35:18 +0200

    Use dephash for production reqs

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+import re
 from setuptools import setup, find_packages
 
 
@@ -9,8 +10,21 @@ with open(os.path.join(project_dir, 'version.txt')) as f:
 
 # We allow commented lines in this file
 with open(os.path.join(project_dir, 'requirements.txt')) as f:
-    requirements = [line.rstrip('\n') for line in f if not line.startswith('#')]
+    requirements_raw = f.read()
 
+try:
+    requirements_without_escaped_end_of_lines = bytes(requirements_raw, 'utf-8').decode('unicode_escape')
+except TypeError:
+    requirements_without_escaped_end_of_lines = requirements_raw.decode('string_escape') # python2
+
+requirements_with_hashes = requirements_without_escaped_end_of_lines.split('\n')
+requirements_without_comments = [
+    line for line in requirements_with_hashes if line and not line.startswith('#')
+]
+requirements_without_hashes = [
+    re.sub(r'--hash=\S+', '', line)
+    for line in requirements_without_comments
+]
 
 setup(
     name='mozilla-version',
@@ -25,7 +39,7 @@ they are nightlies or regular releases, whether this version precedes that other
     include_package_data=True,
     zip_safe=False,
     license='MPL2',
-    install_requires=requirements,
+    install_requires=requirements_without_hashes,
     classifiers=(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
