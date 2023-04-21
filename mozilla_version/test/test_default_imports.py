dec9ba3e7f6e8782e045c7ca33188080cbf01d58
Henrik Skupin <mail@hskupin.info>
2023-04-21 08:32:59 +0200

    Export all public version classes on package level

---
+++ mozilla_version/test/test_default_imports.py
@@ -0,0 +1,14 @@
+import pytest
+
+import mozilla_version
+
+
+@pytest.mark.parametrize('version_type', [
+    'DeveditionVersion',
+    'FirefoxVersion',
+    'MavenVersion',
+    'MobileVersion',
+    'ThunderbirdVersion',
+])
+def test_(version_type):
+    getattr(mozilla_version, version_type)
