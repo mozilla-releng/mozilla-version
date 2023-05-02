892cf8c844df0ca1f11f5a27041d99daa852a625
Henrik Skupin <mail@hskupin.info>
2023-05-02 13:37:33 +0200

    Export all public version classes on package level (#105)

--- mozilla_version/__init__.py
+++ mozilla_version/__init__.py
@@ -1,1 +1,9 @@
 """Defines characteristics of Mozilla's version numbers."""
+
+from mozilla_version.gecko import (
+    DeveditionVersion,
+    FirefoxVersion,
+    ThunderbirdVersion,
+)
+from mozilla_version.maven import MavenVersion
+from mozilla_version.mobile import MobileVersion
