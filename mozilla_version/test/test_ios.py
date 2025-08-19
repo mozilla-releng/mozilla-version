a0bdc58fa24893122ed429122bed95f623ae81d8
Bastien Orivel <borivel@mozilla.com>
2025-08-19 15:44:58 +0200

    Bug 1983816 - Allow patch_number in firefox iOS versions
    Since the weekly releases are a thing, this is required to be able to
    release hotfixes. It also means that the bump shipit was doing was
    wrong since it would bump 142.1 to 142.2 on the 142.1 branch but that
    whole part got missed when the switch to said weekly releases happened.

--- mozilla_version/test/test_ios.py
+++ mozilla_version/test/test_ios.py
@@ -1,5 +1,6 @@
 import pytest
 
+from mozilla_version.errors import PatternNotMatchedError
 from mozilla_version.ios import MobileIosVersion
 
 
@@ -8,16 +9,17 @@ from mozilla_version.ios import MobileIosVersion
     (
         ("0.9", "major_number", "1.0"),
         ("0.9", "major_number", "1.0"),
-        ("1.0", "patch_number", "1.1"),
+        ("1.0", "patch_number", "1.0.1"),
         ("1.5", "minor_number", "1.6"),
         ("2.1", "major_number", "3.0"),
         ("2.0", "major_number", "3.0"),
         ("2.0", "minor_number", "2.1"),
-        ("2.0", "patch_number", "2.1"),
-        ("2.1", "patch_number", "2.2"),
+        ("2.0", "patch_number", "2.0.1"),
+        ("2.0.1", "patch_number", "2.0.2"),
+        ("2.1", "patch_number", "2.1.1"),
         ("2.0b0", "beta_number", "2.0b1"),
         ("2.0b1", "beta_number", "2.0b2"),
-        ("2.0b1", "patch_number", "2.1"),
+        ("2.0b1", "patch_number", "2.0.1"),
     ),
 )
 def test_ios_version_bump(version_string, field, expected):
@@ -48,6 +50,11 @@ def test_ios_version_beta(version_string, expected_beta):
     "version,other,expected_result",
     (
         ("139.0", "139.0", 0),
+        ("139.0.1", "139.0", 1),
+        ("139.0.2", "139.0.1", 1),
+        ("139.0.1", "139.0.1", 0),
+        ("139.0", "139.0.1", -1),
+        ("139.0b1", "139.0.1", -1),
         ("139.1", "139.0", 1),
         ("139.1", "140.0", -1),
         ("139.0b0", "139.0b0", 0),
@@ -62,3 +69,23 @@ def test_ios_version_ordering(version, other, expected_result):
     other = MobileIosVersion.parse(other)
 
     assert version._compare(other) == expected_result
+
+
+@pytest.mark.parametrize(
+    "version,should_raise",
+    (
+        ("139.0", False),
+        ("139.0.0", True),
+        ("139.0.1", False),
+        ("139.0.1b1", True),
+        ("139.0.0b1", True),
+        ("139.0b1", False),
+    ),
+)
+def test_ios_validation(version, should_raise):
+    if should_raise:
+        with pytest.raises(PatternNotMatchedError):
+            MobileIosVersion.parse(version)
+    else:
+        version_obj = MobileIosVersion.parse(version)
+        assert version_obj is not None
